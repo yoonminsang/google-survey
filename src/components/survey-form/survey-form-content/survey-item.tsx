580eccba742edb50bedf6361010483d86434ddb1
yoonminsang <apdlyooapdl@naver.com>
2022-02-06 14:22:15 +0900

    feat: survey item 컴포넌트에 item list 컴포넌트 추가(type별로)

--- src/components/survey-form/survey-form-content/survey-item.tsx
+++ src/components/survey-form/survey-form-content/survey-item.tsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import { FilledInput, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import { FilledInput } from '@mui/material';
 import SurveyFormItmeWrapper from '../common/sruvey-form-item-wrapper';
 import { TSurveyType } from '@/types/survey';
 import SurveyItemMenu from './survey-item-menu';
 import SurveyItemSelect from './survey-item-select';
+import SurveyItemSimpleList from './survey-item-simple-list';
+import SurveyItemComplexList from './survey-item-complex-list';
+import SurveyItemDropdownList from './survey-item-dropdown-list';
 
 const Wrapper = styled(SurveyFormItmeWrapper)`
   margin-top: 12px;
@@ -17,17 +20,14 @@ const DrapWrapper = styled.div`
 const Flex = styled.div`
   display: flex;
   justify-content: space-between;
+  margin-left: 24px;
+  margin-right: 24px;
+  margin-bottom: 24px;
 `;
 
 const Title = styled(FilledInput)`
   width: 446px;
   background-color: transparent;
-`;
-
-const Item = styled.div`
-  margin-left: 24px;
-  margin-right: 24px;
-  margin-bottom: 24px;
 `;
 
 interface IProps {
@@ -38,17 +38,29 @@ interface IProps {
   isSelected: boolean;
   isNeccessary: boolean;
 }
+
+const getList = (type: TSurveyType, data: string[], isSelected: boolean) => {
+  switch (type) {
+    case 'short':
+    case 'long':
+      return <SurveyItemSimpleList type={type} data={data} isSelected={isSelected} />;
+    case 'multiple':
+    case 'checkbox':
+      return <SurveyItemComplexList type={type} data={data} isSelected={isSelected} />;
+    case 'dropdown':
+      return <SurveyItemDropdownList data={data} isSelected={isSelected} />;
+  }
+};
+
 const SurveyItem: React.FC<IProps> = ({ id, type, title, data, isSelected, isNeccessary }) => {
   return (
     <Wrapper>
       <DrapWrapper></DrapWrapper>
-      <Item>
-        <Flex>
-          <Title inputComponent="textarea" value={title} readOnly={!isSelected} />
-          <SurveyItemSelect isSelected={isSelected} />
-        </Flex>
-        {data}
-      </Item>
+      <Flex>
+        <Title inputComponent="textarea" value={title} readOnly={!isSelected} />
+        <SurveyItemSelect isSelected={isSelected} />
+      </Flex>
+      {getList(type, data, isSelected)}
       {isSelected && <SurveyItemMenu id={id} isNeccessary={isNeccessary} />}
     </Wrapper>
   );
