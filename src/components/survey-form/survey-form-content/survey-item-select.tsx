3cf0f1df62a4bef23344161dfe43961d3bc16df5
yoonminsang <apdlyooapdl@naver.com>
2022-02-07 07:29:19 +0900

    feat: survey select에 props적용, 이벤트 핸들링

--- src/components/survey-form/survey-form-content/survey-item-select.tsx
+++ src/components/survey-form/survey-form-content/survey-item-select.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 import { TSurveyType } from '@/types/survey';
@@ -13,21 +13,25 @@ const Wrapper = styled(FormControl)`
 `;
 
 interface IProps {
+  id: number;
+  type: TSurveyType;
   isSelected: boolean;
+  onChangeSurveyType: (id: number, type: TSurveyType) => void;
 }
-const SurveyItemSelect: React.FC<IProps> = ({ isSelected }) => {
-  const [age, setAge] = useState<TSurveyType>('short');
-  const handleChange = (event: any) => {
-    setAge(event.target.value);
-  };
+const SurveyItemSelect: React.FC<IProps> = ({ id, type, isSelected, onChangeSurveyType }) => {
   return (
     <Wrapper className={!isSelected ? 'blind' : ''}>
       <InputLabel id="select-label">타입</InputLabel>
-      <Select labelId="select-label" value={age} label="type" onChange={handleChange}>
+      <Select
+        labelId="select-label"
+        value={type}
+        label="type"
+        onChange={(e) => onChangeSurveyType(id, e.target.value as TSurveyType)}
+      >
         <MenuItem value="short">단답형</MenuItem>
         <MenuItem value="long">장문형</MenuItem>
         <MenuItem value="multiple">객관식</MenuItem>
-        <MenuItem value="chekcbox">체크박스</MenuItem>
+        <MenuItem value="checkbox">체크박스</MenuItem>
         <MenuItem value="dropdown">드롭다운</MenuItem>
       </Select>
     </Wrapper>
