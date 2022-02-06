1d56dab3ea7c2adf79dee89a6a428f71a7164c46
yoonminsang <apdlyooapdl@naver.com>
2022-02-07 07:20:35 +0900

    feat: survey header 훅스 생성(리덕스)

---
+++ src/hooks/useSurveyHeader.ts
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@/store';
+import { changeHeader } from '@/store/survey';
+
+export const useSurveyHeader = () => {
+  const dispatch = useDispatch();
+  const header = useSelector((state: RootState) => state.survey.header);
+  // TODO: usecallback??
+  const onChange = (e: React.ChangeEvent) => {
+    const { value, name } = e.target as HTMLInputElement | HTMLTextAreaElement;
+    dispatch({ type: changeHeader.type, payload: { key: name, value } });
+  };
+  return { header, onChange };
+};
