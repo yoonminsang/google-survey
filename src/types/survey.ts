fece00b3fc54b816b07cd0f5a898af50f84b9f24
yoonminsang <apdlyooapdl@naver.com>
2022-02-07 07:21:18 +0900

    feat: survey header 타입생성

--- src/types/survey.ts
+++ src/types/survey.ts
@@ -1,6 +1,10 @@
 export type TSurveyType = 'short' | 'long' | 'multiple' | 'checkbox' | 'dropdown';
 export type TSurvey = { id: number; type: TSurveyType; title: string; data: string[]; isNeccessary: boolean };
-export interface ISurveys {
+export interface ISurvey {
   surveys: TSurvey[];
   selected: null | number;
+}
+export interface ISurveyHeader {
+  title: string;
+  description: string;
 }
