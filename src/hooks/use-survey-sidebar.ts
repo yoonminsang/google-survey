e6f4e85d9907404b8e5bb80162d59c7107e89833
yoonminsang <apdlyooapdl@naver.com>
2022-02-07 10:37:45 +0900

    feat: use sidebar 훅스 생성

---
+++ src/hooks/use-survey-sidebar.ts
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@/store';
+import { addSurvey } from '@/store/survey';
+
+export const useSurveySidebar = () => {
+  const dispatch = useDispatch();
+  const surveys = useSelector((state: RootState) => state.survey.surveys);
+  // TODO: usecallback??
+  const onAddSurvey = () => {
+    const maxId = Math.max(...surveys.map((survey) => survey.id));
+    const nextSurvey = {
+      id: maxId + 1,
+      type: 'multiple',
+      title: '',
+      data: ['옵션 1'],
+      isNeccessary: false,
+    };
+    dispatch({ type: addSurvey.type, payload: nextSurvey });
+  };
+  return { onAddSurvey };
+};
