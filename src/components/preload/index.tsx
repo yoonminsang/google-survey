e84722ff62a340a711fd29928dff9ee873b65141
yoonminsang <apdlyooapdl@naver.com>
2022-02-08 15:41:37 +0900

    feat: preload 버튼 (제출, 양식지우기)추가

--- src/components/preload/index.tsx
+++ src/components/preload/index.tsx
@@ -1,3 +1,6 @@
+import { useAnswer } from '@/hooks/use-answer';
+import { usePreloadInit } from '@/hooks/use-preload-init';
+import { Button } from '@mui/material';
 import React from 'react';
 import styled from 'styled-components';
 import PreloadContent from './preload-content';
@@ -7,11 +10,27 @@ const Wrapper = styled.div`
   width: 770px;
 `;
 
+const Flex = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 15px;
+`;
+
 const Preload = () => {
+  const { onAddAnswer } = useAnswer();
+  const { onInit } = usePreloadInit();
   return (
     <Wrapper>
       <PreloadHeader />
       <PreloadContent />
+      <Flex>
+        <Button variant="contained" type="button" onClick={onAddAnswer} sx={{ width: '76.5px' }}>
+          제출
+        </Button>
+        <Button variant="text" type="button" onClick={onInit}>
+          양식지우기
+        </Button>
+      </Flex>
     </Wrapper>
   );
 };
